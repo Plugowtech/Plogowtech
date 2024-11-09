a900f39b3f06e49be4721c8b13fbdc20ffa6d02d
cassio-ares <cassiotadeu.ads@gmail.com>
2024-11-09 11:37:22 -0300

    nav + header

--- web-partnership/src/components/Header.jsx
+++ web-partnership/src/components/Header.jsx
@@ -1,30 +1,36 @@
-import React from "react";
+import bgHeader from "../assets/bg-header.png";
 
 export const Header = () => {
   return (
     <header
       className="w-full absolute z-0 top-0 h-[500px] bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: 'url("/banner_header.svg")' }}
+      style={{ backgroundImage: `url(${bgHeader})` }}
     >
-      <div>
-        <h1>
-          Bem-vindo ao<span>PARTNERSHIP</span>
+      <div className="container w-[800px] relative top-[150px] left-[200px] ">
+        <h1 className="text-4xl font-bold mb-6">
+          Bem-vindo ao <span className="text-primary">PARTNERSHIP</span>
         </h1>
+
         <p>
-          <strong>PROFISSIONAIS</strong> : (Arquitetos, Engenheiros, Designers,
+          <strong>PROFISSIONAIS</strong>: (Arquitetos, Engenheiros, Designers,
           Construtores, Paisagistas e outros);
         </p>
-        <p>
-          O <strong>PARTNERSHIP</strong> é mais do que um programa de
-          recompensas; é uma <strong>experiência</strong> de fidelização pensada
-          para fortalecer<strong>parcerias</strong> entre
-          <strong>profissionais</strong>, consultores e nossas lojas parceiras.
-          Neste <strong>sistema</strong>, cada participante acumula pontos
-          valiosos que podem ser usados livremente para alcançar benefícios,
-          criando um ciclo de <strong>reconhecimento e engajamento</strong>
-          genuíno.
-          <button className="btn-home">Quero fazer parte</button>
-        </p>
+
+        <div className="max-w-3xl">
+          <p>
+            O <strong>PARTNERSHIP</strong> é mais do que um programa de
+            recompensas; é uma <strong>experiência</strong> de fidelização
+            pensada para fortalecer
+            <strong>parcerias</strong> entre <strong>profissionais</strong>,
+            consultores e nossas lojas parceiras. Neste <strong>sistema </strong>
+            , cada participante acumula pontos valiosos que podem ser usados
+            livremente para alcançar benefícios, criando um ciclo de
+            <strong>reconhecimento e engajamento </strong>
+            genuíno.
+          </p>
+
+          <button className="btn-home mt-5">Quero fazer parte</button>
+        </div>
       </div>
     </header>
   );
--- web-partnership/src/components/Nav.jsx
+++ web-partnership/src/components/Nav.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+
 
 export const Nav = () => {
   return (
-    <nav className="w-full h-[65px] fixed top-0 bg-transparent bg-opacity-50 z-10 flex items-center justify-between pr-[80px] pt-4">
+    <nav className="w-full pb-4 fixed top-0 bg-gray-900 bg-opacity-50 z-10 flex items-center justify-between pr-[80px] pt-4">
       <div className="w-[600px] h-[45px] m-auto flex items-center justify-around pt-2">
         <a href="#">Sobre nós</a>
         <a href="#">Premiações</a>
