7d024bac7673c6700d1a4a36efd063fdbcdb055c
cassio-ares <cassiotadeu.ads@gmail.com>
2024-11-09 13:57:07 -0300

    branch leonan

--- web-partnership/src/components/Input.jsx
+++ web-partnership/src/components/Input.jsx
@@ -1,10 +1,20 @@
 
 
-export const Input = ({}) => {
+export const Input = () => {
   return (
     <div className="input-container">
-      <input className="input" type="text" placeholder="" />
-      <label className="input-label" htmlFor="">oiiii</label>
+      <input
+        className="input"
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+        required={required}
+        placeholder=""
+      />
+      <label className="input-label" htmlFor="">
+        
+      </label>
     </div>
   );
 }
